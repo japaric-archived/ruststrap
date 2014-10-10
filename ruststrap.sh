19ae1043b4551ffe10e1f1ba486f3f423bc526c1
Gleb Kozyrev <gleb@gkoz.com>
2014-10-10 22:41:03 +0300

    Put target strings into TARGET and TOOLCHAIN_TARGET

--- ruststrap.sh
+++ ruststrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Cross boostraps an arm-unknown-linux-gnueabihf rust compiler
+# Cross boostraps an arm rust compiler
 
 # Based on the blog post "Cross bootstrapping Rust" by Riad Wahby
 # (http://github.jfet.org/Rust_cross_bootstrapping.html)
@@ -12,6 +12,8 @@ set -x
 
 : ${SRC_DIR:=/rust}
 : ${DIST_DIR:=/dist}
+: ${TARGET:=arm-unknown-linux-gnueabihf}
+: ${TOOLCHAIN_TARGET:=arm-linux-gnueabihf}
 
 # Make sure timezone is UTC (just like bors)
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
@@ -30,7 +32,7 @@ fi
 # Get information about HEAD
 HEAD_HASH=$(git rev-parse --short HEAD)
 HEAD_DATE=$(date -d @$(git show -s --format=%ct HEAD) +'%Y-%m-%d')
-TARBALL=rust-${HEAD_DATE}-${HEAD_HASH}-arm-unknown-linux-gnueabihf
+TARBALL=rust-${HEAD_DATE}-${HEAD_HASH}-${TARGET}
 
 # Install ARM cross-compiler
 apt-get install -qq g++-arm-linux-gnueabihf
@@ -44,9 +46,9 @@ cd build
 ../configure \
     --build=x86_64-unknown-linux-gnu \
     --host=x86_64-unknown-linux-gnu \
-    --target=x86_64-unknown-linux-gnu,arm-unknown-linux-gnueabihf
+    --target=x86_64-unknown-linux-gnu,${TARGET}
 cd x86_64-unknown-linux-gnu
-find . -type d -exec mkdir -p ../arm-unknown-linux-gnueabihf/\{\} \;
+find . -type d -exec mkdir -p ../${TARGET}/\{\} \;
 
 # Building cross LLVM
 cd "$SRC_DIR"/build/x86_64-unknown-linux-gnu/llvm
@@ -64,7 +66,7 @@ cd "$SRC_DIR"/build/x86_64-unknown-linux-gnu/llvm
     --host=x86_64-unknown-linux-gnu \
     --target=x86_64-unknown-linux-gnu
 make -j$(nproc)
-cd "$SRC_DIR"/build/arm-unknown-linux-gnueabihf/llvm
+cd "$SRC_DIR"/build/${TARGET}/llvm
 "$SRC_DIR"/src/llvm/configure \
     --enable-targets=x86,x86_64,arm,mips \
     --enable-optimized \
@@ -76,21 +78,22 @@ cd "$SRC_DIR"/build/arm-unknown-linux-gnueabihf/llvm
     --disable-libffi \
     --with-python=/usr/bin/python2.7 \
     --build=x86_64-unknown-linux-gnu \
-    --host=arm-linux-gnueabihf \
-    --target=arm-linux-gnueabihf
+    --host=${TOOLCHAIN_TARGET} \
+    --target=${TOOLCHAIN_TARGET}
 make -j$(nproc)
 
 # Enable llvm-config for the cross build
-cd "$SRC_DIR"/build/arm-unknown-linux-gnueabihf/llvm/Release+Asserts/bin
+cd "$SRC_DIR"/build/${TARGET}/llvm/Release+Asserts/bin
 mv llvm-config llvm-config-arm
 ln -s ../../BuildTools/Release+Asserts/bin/llvm-config
 ./llvm-config --cxxflags
 
 # Making Rust Build System use our LLVM build
+TGT_STR=`echo ${TARGET} | sed 's/-/\\\\1/g'`
 cd "$SRC_DIR"/build/
 chmod 0644 config.mk
 grep 'CFG_LLVM_[BI]' config.mk |                                          \
-    sed 's/x86_64\(.\)unknown.linux.gnu/arm\1unknown\1linux\1gnueabihf/g' \
+    sed "s/x86_64\(.\)unknown.linux.gnu/$TGT_STR/g" \
     >> config.mk
 
 cd "$SRC_DIR"
@@ -102,7 +105,7 @@ sed -i.bak \
     's/^CRATES := .*/TARGET_CRATES += $(HOST_CRATES)\nCRATES := $(TARGET_CRATES)/' \
     mk/crates.mk
 sed -i.bak \
-    's/\(.*call DEF_LLVM_VARS.*\)/\1\n$(eval $(call DEF_LLVM_VARS,arm-unknown-linux-gnueabihf))/' \
+    's/\(.*call DEF_LLVM_VARS.*\)/\1\n$(eval $(call DEF_LLVM_VARS,'${TARGET}'))/' \
     mk/main.mk
 sed -i.bak 's/foreach host,$(CFG_HOST)/foreach host,$(CFG_TARGET)/' mk/rustllvm.mk
 
@@ -110,7 +113,7 @@ cd "$SRC_DIR"
 sed -i.bak 's/.*target_arch = .*//' src/etc/mklldeps.py
 
 cd "$SRC_DIR"/build
-arm-unknown-linux-gnueabihf/llvm/Release+Asserts/bin/llvm-config --libs \
+${TARGET}/llvm/Release+Asserts/bin/llvm-config --libs \
     | tr '-' '\n' | sort > arm
 x86_64-unknown-linux-gnu/llvm/Release+Asserts/bin/llvm-config --libs \
     | tr '-' '\n' | sort > x86
@@ -124,25 +127,25 @@ make -j$(nproc)
 cd "$SRC_DIR"/build
 LD_LIBRARY_PATH=$PWD/x86_64-unknown-linux-gnu/stage2/lib/rustlib/x86_64-unknown-linux-gnu/lib:$LD_LIBRARY_PATH \
     ./x86_64-unknown-linux-gnu/stage2/bin/rustc --cfg stage2 -O --cfg rtopt                                    \
-    -C linker=arm-linux-gnueabihf-g++ -C ar=arm-linux-gnueabihf-ar -C target-feature=+v6,+vfp2                 \
-    --cfg debug -C prefer-dynamic --target=arm-unknown-linux-gnueabihf                                         \
-    -o x86_64-unknown-linux-gnu/stage2/lib/rustlib/arm-unknown-linux-gnueabihf/bin/rustc --cfg rustc           \
+    -C linker=${TOOLCHAIN_TARGET}-g++ -C ar=${TOOLCHAIN_TARGET}-ar -C target-feature=+v6,+vfp2                 \
+    --cfg debug -C prefer-dynamic --target=${TARGET}                                         \
+    -o x86_64-unknown-linux-gnu/stage2/lib/rustlib/${TARGET}/bin/rustc --cfg rustc           \
     $PWD/../src/driver/driver.rs
 LD_LIBRARY_PATH=$PWD/x86_64-unknown-linux-gnu/stage2/lib/rustlib/x86_64-unknown-linux-gnu/lib:$LD_LIBRARY_PATH \
     ./x86_64-unknown-linux-gnu/stage2/bin/rustc --cfg stage2 -O --cfg rtopt                                    \
-    -C linker=arm-linux-gnueabihf-g++ -C ar=arm-linux-gnueabihf-ar -C target-feature=+v6,+vfp2                 \
-    --cfg debug -C prefer-dynamic --target=arm-unknown-linux-gnueabihf                                         \
-    -o x86_64-unknown-linux-gnu/stage2/lib/rustlib/arm-unknown-linux-gnueabihf/bin/rustdoc --cfg rustdoc       \
+    -C linker=${TOOLCHAIN_TARGET}-g++ -C ar=${TOOLCHAIN_TARGET}-ar -C target-feature=+v6,+vfp2                 \
+    --cfg debug -C prefer-dynamic --target=${TARGET}                                         \
+    -o x86_64-unknown-linux-gnu/stage2/lib/rustlib/${TARGET}/bin/rustdoc --cfg rustdoc       \
     $PWD/../src/driver/driver.rs
 
 # Ship it
-mkdir -p "$DIST_DIR"/lib/rustlib/arm-unknown-linux-gnueabihf
+mkdir -p "$DIST_DIR"/lib/rustlib/${TARGET}
 cd "$DIST_DIR"
-cp -R "$SRC_DIR"/build/x86_64-unknown-linux-gnu/stage2/lib/rustlib/arm-unknown-linux-gnueabihf/* \
-    lib/rustlib/arm-unknown-linux-gnueabihf
-mv lib/rustlib/arm-unknown-linux-gnueabihf/bin .
+cp -R "$SRC_DIR"/build/x86_64-unknown-linux-gnu/stage2/lib/rustlib/${TARGET}/* \
+    lib/rustlib/${TARGET}
+mv lib/rustlib/${TARGET}/bin .
 cd lib
-for i in rustlib/arm-unknown-linux-gnueabihf/lib/*.so; do
+for i in rustlib/${TARGET}/lib/*.so; do
   ln -s $i .
 done
 cd ..
